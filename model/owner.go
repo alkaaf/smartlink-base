9764087ae0543b49b17ca21222475a0dee4c22af
Agestyarp <agestyadoank@gmail.com>
2019-10-23 09:06:36 +0800

    tambah model kota edit model owner

--- model/owner.go
+++ model/owner.go
@@ -16,17 +16,17 @@ type Owner struct {
 	KodeVerifikasiTelp  string     `json:"kode_verifikasi_telp"`
 	LevelAfiliasi       int        `json:"level_afiliasi"`
 	KodeAfiliasi        string     `json:"kode_afiliasi"`
-	kodeAfiliator       string     `json:"kode_afiliator"`
+	KodeAfiliator       string     `json:"kode_afiliator"`
 	PersentaseAfiliator int        `json:"persentase_afiliator"`
 	SaldoAfiliator      float32    `json:"saldo_afiliator"`
 	TutorNumber         int        `json:"tutor_number"`
 	DataGenerated       int        `json:"data_generated"`
-	ExpiryTrialDate     *time.Time `json:"expiry_trial_date"`
+	ExpiryTrialDate     time.Time  `json:"expiry_trial_date"`
 	Active              int        `json:"active"`
 	LastLogin           *time.Time `json:"last_login"`
 	EmailReminders      string     `json:"email_reminders"`
 	Versi               string     `json:"versi"`
-	DateReset           string     `json:"date_reset"`
+	DateReset           time.Time  `json:"date_reset"`
 	SmsPremium          int        `json:"sms_premium"`
 	UploadToken         string     `json:"upload_token"`
 	LastKoinNotif       int        `json:"last_koin_notif"`
